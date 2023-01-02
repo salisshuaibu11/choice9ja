a93b97710e42d0f68182181d5b4372447aaed493
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-01-02 18:26:38 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'the-choice9ja-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'the-choice9ja';
+}
